8b882a0f6ca129fe1468b625e89c3d54a6f009c4
lyqgzbl <lyqgzbl@outlook.com>
2024-09-11 00:35:53 +0000

    添加到远程

--- nonebot_plugin_githubmodels/__init__.py
+++ nonebot_plugin_githubmodels/__init__.py
@@ -75,7 +75,7 @@ async def handle_function(args: Message = CommandArg()):
 
 __plugin_meta__ = PluginMetadata(
     name="githubmobile",
-    description="API 调用 GitHub Mobile 的 GPT-4o 模型",
+    description="API 调用 GitHub Models 的 GPT-4o 模型",
     type="application",
     homepage="https://github.com/lyqgzbl/nonebot-plugin-githubmobile",
     supported_adapters={"~onebot.v11"},
