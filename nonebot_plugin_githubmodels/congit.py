3719d99a228e8b3fa263fc8c297b2afe6b72839e
lyqgzbl <122811297+lyqgzbl@users.noreply.github.com>
2024-09-17 23:08:22 +0800

    Update congit.py

--- nonebot_plugin_githubmodels/congit.py
+++ nonebot_plugin_githubmodels/congit.py
@@ -1,1 +1,13 @@
-1
+from typing import Optional
+from nonebot import get_plugin_config
+from pydantic import BaseModel, Field
+
+class Config(BaseModel):
+  token: Optional[str] = Field(default=None)
+  model_name: Optional[str] = Field(default=“gpt-4o-mini”)
+  MAX_CONTEXT_LENGTH: Optionsl[int] = Field(defalut=20)
+
+plugin_config: Config = get_plugin_config(Config)
+TOKEN = plugin_config.github_token
+MODEL_NAME = plugin_config.model_name
+MAX_CONTEXT_LENGTH = plugin_config.MAX_CONTEXT_LENGTH
